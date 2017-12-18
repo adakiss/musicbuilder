ac9908c16c0a1564856fa9a8bf32df47623b1b27
adakiss <galaxymini96>
2017-12-18 22:48:41 +0100

    played around with sounds in c#, inaccurate , but plays something

--- MusicBuilder/Program.cs
+++ MusicBuilder/Program.cs
@@ -8,8 +8,23 @@ namespace MusicBuilder
 {
     class Program
     {
+        static System.Media.SoundPlayer player;
+
         static void Main(string[] args)
         {
+            player = new System.Media.SoundPlayer("sounds/CYCdh_AcouKick-20.wav");
+
+            const int tempo = 60;
+            const int length = 30;
+
+            int sleepSec = 60 / tempo;
+            int loops = length / sleepSec;
+
+            for(int i = 0;i < loops; i++)
+            {
+                player.Play();
+                System.Threading.Thread.Sleep(sleepSec * 1000);
+            }
         }
     }
 }
